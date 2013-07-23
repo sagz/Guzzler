4951a1cdb266f86d6a76ea5973dd48f5244baf53
Sagar Savla <sasavla@paypal.com>
2013-07-23 09:53:23 -0700

    clean up

--- downloader.py
+++ downloader.py
@@ -10,6 +10,7 @@ url = 'https://dl.google.com/dl/linux/direct/google-chrome-unstable_current_x86_
 #url = "http://download.thinkbroadband.com/10MB.zip" # testing
 
 time_bound = False
+data_bound = False
 time_limit = 0
 not_enough = True
 rounds = 1000
@@ -51,12 +52,9 @@ semaphore = 0
 
 while not_enough:
 	u = urllib2.urlopen(url)
-	#f = StringIO() #open(file_name, 'wb')
 	meta = u.info()
 	file_size = int(meta.getheaders("Content-Length")[0])
-	#print "Downloading: %s Bytes: %s" % (file_name, file_size)
-	#guzzled_mb += 
-
+	
 	file_size_dl = 0
 	block_sz = 1024*1
 	while True:
@@ -65,10 +63,6 @@ while not_enough:
 	        break
 
 	    file_size_dl += len(buffer)
-	    #f.write(buffer)
-	    #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-	    #status = status + chr(8)*(len(status)+1)
-	    #print status,
 	    current_guzzled = (semaphore*(float(file_size)/(1024*1024)))+(file_size_dl/(1024*1024))
 	    elapsed_time = ((time.time()-start_time)/60)
 
@@ -78,17 +72,8 @@ while not_enough:
 
 	    if time_bound:
 	    	if time.time()-start_time > time_limit:
-				#f.close()
-				#os.remove(file_name)
 				sys.exit(0)
 
-	#f.close()
-
-	# listing directories
-	# print "The dir is: %s" %os.listdir(os.getcwd())
-
-	# removing
-	#os.remove(file_name)
 	semaphore+=1
 
 	if not time_bound:
