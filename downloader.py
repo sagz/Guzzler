b1aeba66405b50a44ce8487cb8a07f281dddce6d
Sagar <github@sagz.in>
2013-10-10 14:25:49 -0700

    productionizing v0.3

--- downloader.py
+++ downloader.py
@@ -4,72 +4,38 @@ import time
 import multiprocessing
 from multiprocessing import Pool, Value
 from ctypes import c_ulong
-#import guzzler
-import argparse
+import guzzler
+import functools
 
-SECONDS_PER_MINUTE = 60
-MINUTES_PER_HOUR = 60
-HOURS_PER_DAY = 24
-
-BYTES_PER_MEGABYTE = 1024 * 1024
-MEGABYTES_PER_GIGABYTE = 1024
-GIGABYTES_PER_TERABYTE = 1024
-
-
-def set_args():
-    parser = argparse.ArgumentParser(description="""Guzzle Away!
-        Guzzler is a tool that will guzzle your internet bandwidth. It does this by downloading 
-        packages from many high-speed servers and discarding them instantly.
-        This is helpful if you want to know the true sustainable bancwidth of your internet.
-        """)
-
-    bound = parser.add_mutually_exclusive_group()
-
-    bound.add_argument("-s", "--seconds", type=int,
-                       help="Number of seconds to guzzle away.\nDefault is forever.")
-    bound.add_argument("-m", "--minutes", type=int,
-                       help="Number of minutes to guzzle away.\nDefault is forever.")
-    bound.add_argument("-hr", "--hours", type=int,
-                       help="Number of hours to guzzle away.\nDefault is forever.")
-    bound.add_argument("-d", "--days", type=int,
-                       help="Number of days to guzzle away.\nDefault is forever.")
-    bound.add_argument("-mb", "--megabytes", type=int,
-                       help="Number of megabytes to download.\nDefault is time-bound to run forever.")
-    bound.add_argument("-gb", "--gigabytes", type=int,
-                       help="Number of gigabytes to download.\nDefault is time-bound to run forever.")
-    bound.add_argument("-tb", "--terabytes", type=int,
-                       help="Number of terabytes to download.\nDefault is time-bound to run forever.")
-    bound.add_argument("-r", "--rounds", type=int,
-                       help="Number of rounds of downloads.\nDefault is 1000")
-
-    parser.add_argument("-p", "--processes", type=int,
-                        help="Number of guzzling processes to run simultaneously.\nDefault is 4")
-
-    args = parser.parse_args()
-
-    if args.seconds:
-        return 'time', args.seconds
-    elif args.minutes:
-        return 'time', args.minutes * SECONDS_PER_MINUTE
-    elif args.hours:
-        return 'time', args.hours * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
-    elif args.days:
-        return 'time', args.days * HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
-
-    elif args.megabytes:
-        return 'data', args.megabytes * BYTES_PER_MEGABYTE
-    elif args.gigabytes:
-        return 'data', args.gigabytes * MEGABYTES_PER_GIGABYTE * BYTES_PER_MEGABYTE
-    elif args.terabytes:
-        return 'data', args.terabytes * GIGABYTES_PER_TERABYTE * MEGABYTES_PER_GIGABYTE * BYTES_PER_MEGABYTE
+PROCESSES = 3
+URLS_FILE = 'urls.txt'
 
 
 def main():
-    #urls = read_urls('urls.txt')
+    urls = guzzler.read_urls(URLS_FILE)
 
-    bound_type, limit = set_args()
+    bound_type, limit = guzzler.set_args()
+    start_time = time.time()
 
-    print(bound_type, limit)
+    p = Pool(PROCESSES)
+    workers = p.map_async(
+        functools.partial(guzzler.guzzle, bound_type, limit, start_time), urls)
+
+    p.close()
+
+    while not workers.ready():
+        megabytes_guzzled = guzzler.total_downloaded_bytes.value / guzzler.BYTES_PER_MEGABYTE
+        minutes_elapsed = ((time.time() - start_time) / guzzler.SECONDS_PER_MINUTE)
+        average_speed = megabytes_guzzled / minutes_elapsed / guzzler.SECONDS_PER_MINUTE
+
+        guzzle_status = "\r%d mb guzzled in %.2f minutes with an average speed of %.2fMB/s." % (
+            megabytes_guzzled, minutes_elapsed, average_speed)
+
+        print(guzzle_status, end="")
+
+    p.join()
+
+    print()
 
 if __name__ == '__main__':
     main()
--- guzzler.py
+++ guzzler.py
@@ -4,6 +4,66 @@ import time
 import multiprocessing
 from multiprocessing import Pool, Value
 from ctypes import c_ulong
+import argparse
+
+total_downloaded_bytes = Value(c_ulong, 0)
+BLOCK_SIZE = 1024 * 1024
+
+SECONDS_PER_MINUTE = 60
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+
+BYTES_PER_MEGABYTE = 1024 * 1024
+MEGABYTES_PER_GIGABYTE = 1024
+GIGABYTES_PER_TERABYTE = 1024
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description="""Guzzle Away!
+        Guzzler is a tool that will guzzle your internet bandwidth. It does this by downloading 
+        packages from many high-speed servers and discarding them instantly.
+        This is helpful if you want to know the true sustainable bancwidth of your internet.
+        """)
+
+    bound = parser.add_mutually_exclusive_group()
+
+    bound.add_argument("-s", "--seconds", type=int,
+                       help="Number of seconds to guzzle away.\nDefault is forever.")
+    bound.add_argument("-m", "--minutes", type=int,
+                       help="Number of minutes to guzzle away.\nDefault is forever.")
+    bound.add_argument("-hr", "--hours", type=int,
+                       help="Number of hours to guzzle away.\nDefault is forever.")
+    bound.add_argument("-d", "--days", type=int,
+                       help="Number of days to guzzle away.\nDefault is forever.")
+    bound.add_argument("-mb", "--megabytes", type=int,
+                       help="Number of megabytes to download.\nDefault is time-bound to run forever.")
+    bound.add_argument("-gb", "--gigabytes", type=int,
+                       help="Number of gigabytes to download.\nDefault is time-bound to run forever.")
+    bound.add_argument("-tb", "--terabytes", type=int,
+                       help="Number of terabytes to download.\nDefault is time-bound to run forever.")
+    bound.add_argument("-r", "--rounds", type=int,
+                       help="Number of rounds of downloads.\nDefault is 1000")
+
+    parser.add_argument("-p", "--processes", type=int,
+                        help="Number of guzzling processes to run simultaneously.\nDefault is 4")
+
+    args = parser.parse_args()
+
+    if args.seconds:
+        return 'time', args.seconds
+    elif args.minutes:
+        return 'time', args.minutes * SECONDS_PER_MINUTE
+    elif args.hours:
+        return 'time', args.hours * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
+    elif args.days:
+        return 'time', args.days * HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
+
+    elif args.megabytes:
+        return 'data', args.megabytes * BYTES_PER_MEGABYTE
+    elif args.gigabytes:
+        return 'data', args.gigabytes * MEGABYTES_PER_GIGABYTE * BYTES_PER_MEGABYTE
+    elif args.terabytes:
+        return 'data', args.terabytes * GIGABYTES_PER_TERABYTE * MEGABYTES_PER_GIGABYTE * BYTES_PER_MEGABYTE
 
 
 def read_urls(filename):
@@ -20,146 +80,38 @@ def read_urls(filename):
             urls.append(line.strip())
     return urls
 
-time_bound = False
-time_limit = 0
 
-data_bound = False
-data_limit = 0
-
-not_enough = True
-rounds = 1000
-
-
-if len(sys.argv) > 1:
-    if sys.argv[1][-1] == 's' or sys.argv[1][-1] == 'S':
-        try:
-            time_bound = True
-            time_limit = int(sys.argv[1][0:-1])
-            print('Guzzler started for %d seconds' % int(sys.argv[1][0:-1]))
-        except ValueError:
-            pass
-    elif sys.argv[1][-1] == 'm' or sys.argv[1][-1] == 'M':
-        try:
-            time_bound = True
-            time_limit = int(sys.argv[1][0:-1]) * 60
-            print('Guzzler started for %d minutes' % int(sys.argv[1][0:-1]))
-        except ValueError:
-            pass
-    elif sys.argv[1][-1] == 'h' or sys.argv[1][-1] == 'H':
-        try:
-            time_bound = True
-            time_limit = int(sys.argv[1][0:-1]) * 60 * 60
-            print('Guzzler started for %d hours' % int(sys.argv[1][0:-1]))
-        except ValueError:
-            pass
-    elif sys.argv[1][-1] == 'd' or sys.argv[1][-1] == 'D':
-        try:
-            time_bound = True
-            time_limit = int(sys.argv[1][0:-1]) * 60 * 60 * 24
-            print('Guzzler started for %d days' % int(sys.argv[1][0:-1]))
-        except ValueError:
-            pass
-    elif sys.argv[1][-1] == 'b' or sys.argv[1][-1] == 'B':
-        if sys.argv[1][-2] == 'm' or sys.argv[1][-2] == 'M':
-            try:
-                data_bound = True
-                data_limit = int(sys.argv[1][0:-2])
-                print('Guzzler started for %d megabytes' % int(sys.argv[1][0:-2]))
-            except ValueError:
-                pass
-        elif sys.argv[1][-2] == 'G' or sys.argv[1][-2] == 'G':
-            try:
-                data_bound = True
-                data_limit = int(sys.argv[1][0:-2]) * 1024
-                print('Guzzler started for %d gigabytes' % int(sys.argv[1][0:-2]))
-            except ValueError:
-                pass
-        elif sys.argv[1][-2] == 't' or sys.argv[1][-2] == 'T':
-            try:
-                data_bound = True
-                data_limit = int(sys.argv[1][0:-2]) * 1024 * 1024
-                print('Guzzler started for %d terabytes' % int(sys.argv[1][0:-2]))
-            except ValueError:
-                pass
-    else:
-        try:
-            if int(sys.argv[1]) > 0:
-                rounds = int(sys.argv[1])
-                print('Guzzler started for %d rounds' % int(sys.argv[1]))
-        except ValueError:
-            pass
-
-start_time = time.time()
-
-each_data = []
-total_data = Value(c_ulong, 0)
-
-BLOCK_SIZE = 1024 * 1024
-
-
-def guzzle(fileurl, bound_type, limit, start_time):
+def guzzle(bound_type, limit, start_time, url):
     """
-    Downloads package at url and updates the multi-processing synchronized value 
+    Downloads package at url and updates the multiprocessing-synchronized value "total_downloaded_bytes"
 
-
-
+    url : the file url being fetched
+    bound_type : string with either of two values
+                'data' means guzzling is limited by data
+                'time' mean guzzling is limited by time
+    limit : raw value of limit
+            if bound_type is 'data', limit is the number of bytes
+            if bound_type is 'time', limit is the number of seconds
+    start_time : time when guzzling started
     """
-    round_number = 0
-    global not_enough, data_limit, data_bound, time_bound, time_limit, rounds, start_time, total_data, BLOCK_SIZE
+    global total_downloaded_bytes
 
-    #each_data[multiprocessing.current_process().name] = 0
-    while not_enough:
+    # parent loop which downloads the same url package till limit is crossed.
+    while (bound_type == 'data' and total_downloaded_bytes.value < limit) or (bound_type == 'time' and time.time() - start_time < limit):
+
         try:
-            u = urllib.request.urlopen(fileurl)
+            u = urllib.request.urlopen(url)
         except urllib.error.URLError:
             print('\rInternet problems, bro')
             continue
 
-        file_size = int(u.getheader("Content-Length"))
+        # child loop which downloads partial blocks of the url package till url
+        # package is completely downloaded
+        while (bound_type == 'data' and total_downloaded_bytes.value < limit) or (bound_type == 'time' and time.time() - start_time < limit):
 
-        file_size_dl = 0
+            temp_buffer = u.read(BLOCK_SIZE)
 
-        while True:
-            temp_buffer = u.read(block_sz)
             if not temp_buffer:
                 break
 
-            file_size_dl += len(temp_buffer)
-
-            current_guzzled = (round_number * (float(file_size) / (1024 * 1024))) + (
-                file_size_dl / (1024 * 1024))
-
-            #each_data[multiprocessing.current_process().name] += (file_size_dl/(1024*1024))
-            total_data.value += int(len(buffer) / 1024 / 1024)  # file_size_dl/1024/1024
-
-            if bound_type == 'data':
-                if current_guzzled > limit:
-                    not_enough = False
-                    break  # sys.exit(0)
-
-            elif bound_type == 'time':
-                if time.time() - start_time > limit:
-                    not_enough = False
-                    break  # sys.exit(0)
-
-        round_number += 1
-
-        if not time_bound:
-            if round_number >= rounds:
-                not_enough = False  # that is, enough is enough.
-
-p = Pool(3)
-workers = p.map_async(guzzle, urls)
-
-p.close()
-
-while not workers.ready():
-    megabytes_guzzled = total_data.value
-    minutes_elapsed = ((time.time() - start_time) / 60)
-    average_speed = megabytes_guzzled / minutes_elapsed / 60
-
-    guzzle_status = "\r%d mb guzzled in %.2f minutes with an average speed of %.2fMB/s." % (
-        megabytes_guzzled, minutes_elapsed, average_speed)
-    print(guzzle_status, end="")
-
-p.join()
+            total_downloaded_bytes.value += len(temp_buffer)
